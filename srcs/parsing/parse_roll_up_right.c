fc1556ed2d0eb8755491a1df1f84ae6ae7cd8800
Thomas OLIVER <toliver@e1r7p3.42.fr>
2018-12-16 18:11:01 +0100

    rot_zaxis function

--- srcs/parsing/parse_roll_up_right.c
+++ srcs/parsing/parse_roll_up_right.c
@@ -6,11 +6,19 @@
 /*   By: cvermand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 15:24:53 by cvermand          #+#    #+#             */
-/*   Updated: 2018/12/16 17:58:44 by cvermand         ###   ########.fr       */
+/*   Updated: 2018/12/16 18:10:47 by toliver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+t_vec			vec_rot_zaxis_angle(t_vec v, t_vec zaxis, float angle)
+{
+	t_matrix	rotmatrix;
+
+	rotmatrix = rotmatrix_axis_angle(zaxis, -angle);
+	return (matrix_mult_vec(rotmatrix, v)); 	
+}
 
 static t_obj	*up_right_roll_all_given(t_obj *obj)
 {
@@ -85,11 +93,11 @@ t_obj	*parse_roll_up_right(t_elem *elem, t_obj *obj)
 	float	minUp;
 	float	minRight;
 	
-/*	if (elem != NULL)
+	if (elem != NULL)
 	{
 		obj->up = parse_vector(find_elem_by_key(elem, "up"));
 		obj->right = parse_vector(find_elem_by_key(elem, "right"));
-		roll = parse_float(find_elem_by_key(elem, "roll"));
+		roll = parse_degree_to_rad(parse_float(find_elem_by_key(elem, "roll")));
 		minUp = min_of_vec(obj->up);
 		minRight = min_of_vec(obj->right);
 		if (minUp == INFINITY && minRight == INFINITY && roll == INFINITY)
@@ -108,6 +116,6 @@ t_obj	*parse_roll_up_right(t_elem *elem, t_obj *obj)
 		obj->up = vec_init0(0.0, 1.0, 0.0);
 		obj->right = vec_init0(1.0, 0.0, 0.0);
 		obj->roll = 0.0;
-	}*/
+	}
 	return (obj);
 }
